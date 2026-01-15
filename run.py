a57c6e5a19763b018c2eaf13951c941b6c2bc99f
Sinister-God008 <145799432+Sinister-God008@users.noreply.github.com>
2026-01-16 00:57:22 +0530

    Refactor run.py to simplify app initialization
    Removed database creation and app context setup.

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-from PasswordVaultApp import create_app,db
-app=create_app()
-with app.app_context():
-    db.create_all()
-if __name__=="__main__":
-    app.run(debug=True)
+from PasswordVaultApp import create_app
+
+app = create_app()
